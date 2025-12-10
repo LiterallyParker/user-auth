dd444f74b117c51ca0c888ee55c3f07f28113449
LiterallyParker <parkernash2001@gmail.com>
2025-12-10 14:30:42 -0600

    minor changes

--- cron/index.js
+++ cron/index.js
@@ -1,8 +1,12 @@
 const scheduler = require("./scheduler")
 const jobs = require("./jobs");
+const { ANSIcolors } = require("../util");
 
 module.exports = () => {
     const tasks = scheduler(jobs);
     console.log(`Started ${tasks.length} cron jobs`);
+    for (const job of jobs) {
+        console.log(`  ${ANSIcolors.magenta}${job.name}${ANSIcolors.reset}`)
+    }
     return tasks;
 };
