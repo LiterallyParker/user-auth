09ef0619a573ba0d4fda8d34c754491fc1adf4b3
= <=>
2025-12-20 16:25:17 -0600

    cleanUsedTokens cron job

--- src/cron/jobs/cleanUsedTokens.js
+++ src/cron/jobs/cleanUsedTokens.js
@@ -1,10 +1,14 @@
-const { deleteToken } = require("../../database/tokens");
+const { deleteBulkTokens } = require("../../database/tokens");
 
 module.exports = {
     name: "CleanUsedTokens",
     cronExpression: "0 0 * * *", // at midnight
     job: async () => {
-        const expiredRows = await deleteToken({ expiresAt: expiresAt > new Date() });
-        const usedRows = await deleteToken({ usedAt: usedAt !== null });
+        try {
+            const rows = await deleteBulkTokens({ usedAt$notNull: true })
+            console.log(rows)
+        } catch (error) {
+            console.error(error);
+        }
     }
 };
